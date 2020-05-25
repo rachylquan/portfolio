c70ad2a1faef63d1a98ae3721f341687fd8406c0
Rachyl <rachylquan@yahoo.com>
2020-05-24 18:00:14 -0700

    add localpandemic app

--- work.js
+++ work.js
@@ -1,6 +1,15 @@
 const WORK = [
   // web app
   {
+    title: 'localPandemic Web App',
+    tech: ['jQuery', 'Javascript'],
+    imgSrc: 'assets/img/localPandemic.png',
+    description: 'The localPandemic app provides detailed stats and news on the COVID-19 epidemic from the smartable api based off a selected location in the United States. This app shows how I can pull data from an API and display it for users.',
+    projectLink: 'https://rachylquan.github.io/localPandemic-app/',
+    repoLink: 'https://github.com/rachylquan/localPandemic-app',
+  },
+  // web app
+  {
     title: 'interactive web app',
     tech: ['jQuery', 'Javascript'],
     imgSrc: 'assets/img/interactivequizapp.png',
