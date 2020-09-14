bc39e6a773e6df52cabe7cca8bb601ad62ff802e
Rachyl <rachylquan@yahoo.com>
2020-09-14 00:36:26 -0700

    restyle

--- main.js
+++ main.js
@@ -1,26 +1,145 @@
 // create project for each one added
-function createProject() { 
+
+function createProject() {
   const output = [];
-  // html for each project card
-  $.each(WORK, function(index, value) {
-    output.push(
-      `<div class="project-card">
-        <div class="project-img"><img src="${this.imgSrc}" alt="${this.title + ' image'}"></div>
-        <div class="project-copy">
-          <h2>${this.title}</h2>
-          <strong>tech used:</strong><i>${this.tech}</i>
-          <div class="project-info">
-            <p>${this.description}</p>
-            <a class="project-link" href="${this.projectLink}" target="_blank">View Project</a>
-            <a class="project-link" href="${this.repoLink}" target="_blank">View Code</a>
+
+  WORK.forEach((project) => {
+    if (project.repoLink !== '') {
+      output.push(`
+        <div class="project-card">
+          <div class="project-img">
+            <img src="${project.imgSrc}" alt="${project.title + ' image'}">
+          </div>
+          <div class="project-copy">
+            <h2>${project.title}</h2>
+            <strong>tech used: </strong><i>${project.tech.join(', ')}</i>
+            <div class="project-info">
+              <p>${project.description}</p>
+              <a class="project-link" href="${
+                project.projectLink
+              }" target="_blank">View Project</a>
+              <a class="project-link repo-link" href="${
+                project.repoLink
+              }" target="_blank">View Code</a>
+            </div>
           </div>
         </div>
-      </div>`
-    )
+      `);
+    } else {
+      output.push(`
+        <div class="project-card">
+          <div class="project-img">
+            <img src="${project.imgSrc}" alt="${project.title + ' image'}">
+          </div>
+          <div class="project-copy">
+            <h2>${project.title}</h2>
+            <strong>tech used: </strong><i>${project.tech.join(', ')}</i>
+            <div class="project-info">
+              <p>${project.description}</p>
+              <a class="project-link" href="${
+                project.projectLink
+              }" target="_blank">View Project</a>
+            </div>
+          </div>
+        </div>
+      `);
+    }
   });
-  // add it to the end of the work section
+
   $('.work-sec').append(output);
 }
 
-
 $(createProject);
+
+// function createProject() {
+//   const output = [];
+//   // html for each project card
+//   $.each(WORK, function (index, value) {
+//     output.push(
+//       `<div class="project-card">
+//         <div class="project-img"><img src="${this.imgSrc}" alt="${
+//         this.title + ' image'
+//       }"></div>
+//         <div class="project-copy">
+//           <h2>${this.title}</h2>
+//           <strong>tech used:</strong><i>${this.tech}</i>
+//           <div class="project-info">
+//             <p>${this.description}</p>
+//             <a class="project-link" href="${
+//               this.projectLink
+//             }" target="_blank">View Project</a>
+//             <a class="project-link" href="${
+//               this.repoLink
+//             }" target="_blank">View Code</a>
+//           </div>
+//         </div>
+//       </div>`
+//     );
+//     if (this.tech !== '') {
+//       output.push(
+//         `<a class="project-link" href="${this.repoLink}" target="_blank">View Code</a>`
+//       );
+//     }
+//   });
+//   // add it to the end of the work section
+//   $('.work-sec').append(output);
+// }
+
+// $(createProject);
+
+//Sticky menu
+
+var sticky = document.querySelector('.navbar');
+var origOffsetY = sticky.offsetTop;
+
+function onScroll(e) {
+  window.scrollY >= origOffsetY
+    ? sticky.classList.add('fixed')
+    : sticky.classList.remove('fixed');
+}
+document.addEventListener('scroll', onScroll);
+
+// Scroll active
+
+$(window)
+  .scroll(function () {
+    var scrollDistance = $(window).scrollTop();
+
+    $('section').each(function (i) {
+      if ($(this).position().top - 51 <= scrollDistance) {
+        $('a[href*="#"]:not([href="#"]).active').removeClass('active');
+        $('a').eq(i).addClass('active');
+      }
+    });
+  })
+  .scroll();
+
+//Scroll to anchor
+
+$(function () {
+  $('a[href*="#"]:not([href="#"])').click(function () {
+    // scroll active
+
+    $('a').each(function () {
+      $(this).removeClass('active');
+    });
+    $(this).addClass('active');
+    if (
+      location.pathname.replace(/^\//, '') ===
+        this.pathname.replace(/^\//, '') &&
+      location.hostname === this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate(
+          {
+            scrollTop: target.offset().top - 50,
+          },
+          500
+        );
+        return false;
+      }
+    }
+  });
+});
--- work.js
+++ work.js
@@ -1,20 +1,39 @@
 const WORK = [
-  // web app
+  // web app - api
   {
     title: 'localPandemic Web App',
     tech: ['jQuery', 'Javascript'],
     imgSrc: 'assets/img/localPandemic.png',
-    description: 'The localPandemic app provides detailed stats and news on the COVID-19 epidemic from the smartable api based off a selected location in the United States. This app shows how I can pull data from an API and display it for users.',
+    description:
+      'The localPandemic app provides detailed stats and news on the COVID-19 epidemic from the smartable api based off a selected location in the United States. This app shows how I can pull data from an API and display it for users.',
     projectLink: 'https://rachylquan.github.io/localPandemic-app/',
     repoLink: 'https://github.com/rachylquan/localPandemic-app',
   },
-  // web app
   {
-    title: 'interactive web app',
-    tech: ['jQuery', 'Javascript'],
-    imgSrc: 'assets/img/interactivequizapp.png',
-    description: 'This interactive quiz app was completed as part of the Thinkful Full-stack Web Development Course. It showcases my knowledge of semantic HTML, CSS, jQuery, and Javascript. The quiz covers your knowledge of the eight Harry Potter Films.',
-    projectLink: 'https://rachylquan.github.io/quiz-app/',
-    repoLink: 'https://github.com/rachylquan/quiz-app',
-  }
-]
+    title: 'Forever With Us',
+    tech: ['Adobe Creative Cloud', 'Javascript', 'CSS3', 'Squarespace'],
+    imgSrc: 'assets/img/foreverWithUs.png',
+    description:
+      'Established a professional, cohesive brand identity through branding, website styling, and content creation.',
+    projectLink: 'https://www.foreverwus.org/',
+    repoLink: '',
+  },
+  {
+    title: 'AlwaysFriday',
+    tech: ['Adobe Creative Cloud', 'HTML5', 'CSS3', 'Squarespace'],
+    imgSrc: 'assets/img/rq-alwaysFriday.gif',
+    description:
+      'I collaborated with Kara to bring her brand to life on her website and instagram. The goal was to create a cohesive, professional look that fully represents everything she has to offer.',
+    projectLink: 'https://www.alwaysfriday.com/',
+    repoLink: '',
+  },
+  // web app
+  // {
+  //   title: 'interactive web app',
+  //   tech: ['jQuery', 'Javascript'],
+  //   imgSrc: 'assets/img/interactivequizapp.png',
+  //   description: 'This interactive quiz app was completed as part of the Thinkful Full-stack Web Development Course. It showcases my knowledge of semantic HTML, CSS, jQuery, and Javascript. The quiz covers your knowledge of the eight Harry Potter Films.',
+  //   projectLink: 'https://rachylquan.github.io/quiz-app/',
+  //   repoLink: 'https://github.com/rachylquan/quiz-app',
+  // }
+];
